b73c349814019909f5b29a99da30daa010ef00ce
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-02-21 13:00:51 +0100

    Components Starting Adding Comments

--- react-complete/src/components/ExpensItem.js
+++ react-complete/src/components/ExpensItem.js
@@ -1,6 +1,9 @@
-import './ExpensItem.css'
+import './ExpensItem.css'  // import css file
 
 function ExpenseItem() {
+    const ExpanseDate = new Date(2023, 2, 21);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
     return (
         <div className="expense-item">
             <div>March 28th 2021</div>
@@ -11,7 +14,7 @@ function ExpenseItem() {
                 className="expense-item__price">$294.67
             </div>
         </div>
-    );
+    );   // return JSX
 }
 
-export default ExpenseItem;
+export default ExpenseItem; // export component
