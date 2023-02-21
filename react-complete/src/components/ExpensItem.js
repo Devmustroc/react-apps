ace7618fca27dcc17fe2e7a7bcb9ee4e85c665bb
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-02-21 13:06:57 +0100

    Components Starting Adding reusible components

--- react-complete/src/components/ExpensItem.js
+++ react-complete/src/components/ExpensItem.js
@@ -1,20 +1,17 @@
 import './ExpensItem.css'  // import css file
 
 function ExpenseItem() {
-    const ExpanseDate = new Date(2023, 2, 21);
+    const expanseDate = new Date(2023, 2, 21);
     const expenseTitle = 'Car Insurance';
     const expenseAmount = 294.67;
-    return (
-        <div className="expense-item">
-            <div>March 28th 2021</div>
-            <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-            </div>
-            <div
-                className="expense-item__price">$294.67
-            </div>
+    return (<div className="expense-item">
+        <div>{expanseDate.toISOString()}</div>
+        <div className="expense-item__description">
+            <h2>{expenseTitle}</h2>
         </div>
-    );   // return JSX
+        <div className="expense-item__price">${expenseAmount}
+        </div>
+    </div>);   // return JSX
 }
 
 export default ExpenseItem; // export component
