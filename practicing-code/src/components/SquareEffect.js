e1cb810f2312b203bcea47eaa22409f1b6f5a506
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-02-21 13:42:58 +0100

    adding components with some visual

---
+++ practicing-code/src/components/SquareEffect.js
@@ -0,0 +1,15 @@
+import './SquareEffect.css'
+
+function SquareEffect () {
+    return <div className="container">
+        <div className="wrapper">
+            <div className="rubber"></div>
+            <div className="stick left-stick"></div>
+            <div className="stick right-stick"></div>
+            <div className="square">
+            </div>
+        </div>
+    </div>
+}
+
+export default SquareEffect
