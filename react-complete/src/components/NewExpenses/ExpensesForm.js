cd38a5f03c54b0b37327e4b40dbe7d17155f89be
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-02-23 12:03:42 +0100

    Event Handler Using Builtin React useState

--- react-complete/src/components/NewExpenses/ExpensesForm.js
+++ react-complete/src/components/NewExpenses/ExpensesForm.js
@@ -2,39 +2,51 @@ import React, {useState} from 'react';
 import "./ExpenseForm.css";
 
 const ExpensesForm = () => {
-    const [enteredTitle, setEnteredTiltle] = useState('');
+    // const [enteredTitle, setEnteredTiltle] = useState('');
+    // const [enteredDate, setEnteredDate] = useState('');
+    // const [enteredAmount, setEnteredAmount] = useState('');
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',  // This is the initial state
+        enteredDate: '',  // This is the initial state
+        enteredAmount: '' // This is the initial state
+    })  // object destructuring for the state using useState hook
     const titleChangeHandler = (event) => {
-        setEnteredTiltle(event.target.value);
+        setUserInput({
+            ...userInput,  // spread operator for copying the previous state
+            enteredTitle: event.target.value  // This is the new state
+        })
     };
-    const [enteredAmount, setEnteredAmount] = useState('');
     const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
+        setUserInput({
+            ...userInput, // spread operator for copying the previous state
+            enteredAmount: event.target.value // This is the new state
+        })
     }
-    const [enteredDate, setEnteredDate] = useState('');
+
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-        console.log(event.target.value);
+        setUserInput({
+            ...userInput,  // spread operator for copying the previous state
+            enteredDate: event.target.value // This is the new state
+        })
     }
-    return (
-        <form>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
-                </div>
-                <div className='new-expense__control'>
-                    <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
-                </div>
-                <div className='new-expense__control'>
-                    <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2023-2-22" onChange={dateChangeHandler}/>
-                </div>
-                </div>
-            <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
+    return (<form>
+        <div className="new-expense__controls">
+            <div className="new-expense__control">
+                <label>Title</label>
+                <input type="text" onChange={titleChangeHandler}/>
             </div>
-        </form>
-)
+            <div className='new-expense__control'>
+                <label>Amount</label>
+                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+            </div>
+            <div className='new-expense__control'>
+                <label>Date</label>
+                <input type="date" min="2019-01-01" max="2023-2-22" onChange={dateChangeHandler}/>
+            </div>
+        </div>
+        <div className="new-expense__actions">
+            <button type="submit">Add Expense</button>
+        </div>
+    </form>)
 };
 export default ExpensesForm;
