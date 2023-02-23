82d96dd230cbbd7f3879356172a12f7e7c0e31c5
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-02-23 11:56:50 +0100

    Event Handler Using Builtin React useState

--- react-complete/src/components/NewExpenses/ExpensesForm.js
+++ react-complete/src/components/NewExpenses/ExpensesForm.js
@@ -2,10 +2,19 @@ import React, {useState} from 'react';
 import "./ExpenseForm.css";
 
 const ExpensesForm = () => {
-    const [enteredTiltle, setEnteredTiltle] = useState('');
+    const [enteredTitle, setEnteredTiltle] = useState('');
     const titleChangeHandler = (event) => {
         setEnteredTiltle(event.target.value);
     };
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    }
+    const [enteredDate, setEnteredDate] = useState('');
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+        console.log(event.target.value);
+    }
     return (
         <form>
             <div className="new-expense__controls">
@@ -15,11 +24,11 @@ const ExpensesForm = () => {
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01"/>
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2023-2-22"/>
+                    <input type="date" min="2019-01-01" max="2023-2-22" onChange={dateChangeHandler}/>
                 </div>
                 </div>
             <div className="new-expense__actions">
